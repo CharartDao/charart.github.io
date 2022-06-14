da7e6c05a2c0d83ad504f4559c40a2ec456ded6d
ChenpoHU <maodauibe@gmail.com>
2022-06-14 16:22:24 +0200

    responsive update the displayed images

--- src/pages/public/nft-page/nft-page.tsx
+++ src/pages/public/nft-page/nft-page.tsx
@@ -1,7 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import Box from '@mui/material/Box';
 import ImageList from '@mui/material/ImageList';
-import ImageListItem from '@mui/material/ImageListItem';
+import ImageListItem, { imageListItemClasses } from '@mui/material/ImageListItem';
 import ImageListItemBar from '@mui/material/ImageListItemBar';
+import {createTheme, ThemeProvider} from "@mui/material/styles";
 import layout from '../../../components/layout';
 import Asset from '../../../components/asset';
 import './nft-page.scss';
@@ -9,6 +11,39 @@ import Waterbird from '../../../arttemp/Waterbird.JPG';
 import Cat from '../../../arttemp/IMG_0721.JPG';
 import LesserWhiteToothedShrew from '../../../arttemp/IMG_E0842.JPG';
 import BlackWhiteTiger from '../../../arttemp/IMG_0917.JPG';
+
+const breakpoints = {
+  xs: 0,
+  sm: 600,
+  md: 960,
+  lg: 1280,
+  xl: 1920
+  };
+
+const getColumns = (width:any) => {
+  if (width < breakpoints.sm) {
+      return 2
+  } else if (width < breakpoints.md) {
+      return 3
+  } else if (width < breakpoints.lg) {
+      return 6
+  } else if (width < breakpoints.xl) {
+      return 7
+  } else {
+      return 8
+  }
+}
+
+const [columns, setColumns] = useState(getColumns(window.innerWidth))
+const updateDimensions = () => {
+  setColumns(getColumns(window.innerWidth))
+}
+
+useEffect(() => {
+  window.addEventListener("resize", updateDimensions);
+  return () => window.removeEventListener("resize", updateDimensions);
+}, []);
+
 
 const NFTPage: React.FC = () => {
 
@@ -44,7 +79,8 @@ const NFTPage: React.FC = () => {
 	];
 
   return (
-    <ImageList sx={{ width: 500, height: 450 }}>
+    <ImageList cols={columns}
+  >
       {itemData.map((item) => (
         <ImageListItem key={item.imageUrl}>
           <img
@@ -60,8 +96,7 @@ const NFTPage: React.FC = () => {
           />
         </ImageListItem>
       ))}
-    </ImageList>
-  );
+  </ImageList>  );
 };
 
 export default layout(NFTPage)({ pageName: 'NFT' });
