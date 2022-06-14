1f953b419fd17ce371333b0e0e4c0a6fe67e2430
ChenpoHU <maodauibe@gmail.com>
2022-06-14 16:25:08 +0200

    update naming convention

--- src/routes/Route.tsx
+++ src/routes/Route.tsx
@@ -10,15 +10,14 @@ import {
 import LandingPage from '../pages/public/landing-page/landing-page';
 import ArtDetailPage from '../pages/public/art-detail-page/art-detail-page';
 import AboutPage from '../pages/public/about-page/about-page';
-import ServicePage from '../pages/public/nft-page/nft-page';
-import nftPage from '../pages/public/nft-page/nft-page';
+import NFTPage from '../pages/public/nft-page/nft-page';
 
 const Route: React.FC = () => {
   return (
     <Switch>
       {/* Public PAGES */}
       <PublicRoute path={ABOUT} component={AboutPage} />
-      <PublicRoute path={NFT} component={nftPage} />
+      <PublicRoute path={NFT} component={NFTPage} />
       <PublicRoute path={ART_DETAIL_PAGE} component={ArtDetailPage} />
       <PublicRoute path={LANDING} component={LandingPage} />
     </Switch>
