9f52cfdd3856f2bf8c05c04c707fb9f2463d0c5e
ChenpoHU <maodauibe@gmail.com>
2022-06-15 22:01:20 +0200

    update route

--- src/routes/Route.tsx
+++ src/routes/Route.tsx
@@ -10,14 +10,14 @@ import {
 import LandingPage from '../pages/public/landing-page/landing-page';
 import ArtDetailPage from '../pages/public/art-detail-page/art-detail-page';
 import AboutPage from '../pages/public/about-page/about-page';
-import digitalArtPage from '../pages/public/digital-art-page/digital-art-page';
+import DigitalArtPage from '../pages/public/digital-art-page/digital-art-page';
 
 const Route: React.FC = () => {
   return (
     <Switch>
       {/* Public PAGES */}
       <PublicRoute path={ABOUT} component={AboutPage} />
-      <PublicRoute path={DIGITALART} component={digitalArtPage} />
+      <PublicRoute path={DIGITALART} component={DigitalArtPage} />
       <PublicRoute path={ART_DETAIL_PAGE} component={ArtDetailPage} />
       <PublicRoute path={LANDING} component={LandingPage} />
     </Switch>
