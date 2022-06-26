8c63981541aab1823086554d63ab7fb0e4f665e9
ChenpoHU <maodauibe@gmail.com>
2022-06-26 12:37:13 +0200

    debugging

--- src/components/audio-analyser.tsx
+++ src/components/audio-analyser.tsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import AudioVisualiser from './audio-visualiser';
 
 type Props = {
 audio: MediaStream
@@ -9,7 +8,12 @@ type State = {
     audioData: Uint8Array
 };
 
-declare const window: any;
+declare global {
+  interface Window {
+    webkitAudioContext: typeof AudioContext
+    mozAudioContext: typeof AudioContext
+  }
+}
 
 export class AudioAnalyser extends React.PureComponent<Props, State> {
   audioContext!: AudioContext;
@@ -17,16 +21,28 @@ export class AudioAnalyser extends React.PureComponent<Props, State> {
   dataArray!: Uint8Array;
   source!: MediaStreamAudioSourceNode;
   rafId!: number;
+  canvas!: React.RefObject<HTMLCanvasElement>;
 
   constructor(props: Props) {
     super(props);
     this.state = { audioData: new Uint8Array(0) };
+    this.canvas = React.createRef();
     this.tick = this.tick.bind(this);
   }
 
   componentDidMount() {
-    this.audioContext = new (window.AudioContext || window.webkitAudioContext)();
+    this.audioContext = new (window.webkitAudioContext || window.AudioContext || window.mozAudioContext)();
     console.log("audio context", this.audioContext);
+
+    // the AudioContext is the primary 'container' for all your audio node objects
+    if(!this.audioContext) {
+      try {
+          this.audioContext = new AudioContext();
+      } catch(e) {
+          alert('Web Audio API is not supported in this browser');
+      }
+    }
+  
     this.analyser = this.audioContext.createAnalyser();
     console.log("data analyser", this.analyser);
     console.log("data analyser.frequencyBinCount", this.analyser.frequencyBinCount);
@@ -35,6 +51,7 @@ export class AudioAnalyser extends React.PureComponent<Props, State> {
     this.source = this.audioContext.createMediaStreamSource(this.props.audio);
     console.log("this.props.audio", this.props.audio);
     this.source.connect(this.analyser);
+    this.analyser.connect(this.audioContext.destination);
     this.rafId = requestAnimationFrame(this.tick);
   }
 
@@ -42,6 +59,26 @@ export class AudioAnalyser extends React.PureComponent<Props, State> {
     this.analyser.getByteTimeDomainData(this.dataArray!);
     this.setState({ audioData: this.dataArray });
     console.log("audioData", this.state.audioData);
+    const canvas = this.canvas.current;
+    const height = canvas!.height;
+    const width = canvas!.width;
+    const context = canvas!.getContext('2d');
+    let x = 0;
+    const sliceWidth = (width * 1.0) / this.state.audioData.length;
+
+    context!.lineWidth = 2;
+    context!.strokeStyle = '#000000';
+    context!.clearRect(0, 0, width, height);
+
+    context!.beginPath();
+    context!.moveTo(0, height / 2);
+    this.state.audioData.forEach(item => {
+        const y = (item / 255.0) * height;
+        context!.lineTo(x, y);
+        x += sliceWidth;
+    });
+    context!.lineTo(x, height / 2);
+    context!.stroke();
     this.rafId = requestAnimationFrame(this.tick);
   }
 
@@ -52,9 +89,8 @@ export class AudioAnalyser extends React.PureComponent<Props, State> {
   }
 
   render() {
-    return <AudioVisualiser  audioData={this.state.audioData}/>;
+    return <canvas width="300" height="300" ref={this.canvas} />;
   }
 }
 
 export default AudioAnalyser;
-
